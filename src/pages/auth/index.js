b69190b8f5e5232e8ac24cf9ec8b6c0f6dd210ff
wascoyur-swetBook <mytem20007@gmail.com>
2021-04-02 21:11:30 +0500

    008.useState hook

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,39 +1,46 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Auth = () => {
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    console.log(count);
+    setCount(count +1)
+  };
   return (
     <div className='auth-page'>
+      <button onClick={() => handleCount()}>Count {count}</button>
       <div className='container page'>
         <div className='row'>
           <div className='col-md-6 offset-md-3 col-xs-12'>
             <h1 className='text-xs-center'>Вход</h1>
             <p className='text-xs-center'>
               <Link to='/register'>Нужен аккаунт?</Link>
-              <form>
-                <fieldset>
-                  <fieldset className='form-group'>
-                    <input
-                      type='email'
-                      className='form-control form-control-lg'
-                      placeholder='Введите email'
-                    ></input>
-                  </fieldset>
-                  <fieldset className='form-group'>
-                    <input
-                      type='password'
-                      className='form-control form-control-lg'
-                      placeholder='Введите пароль'
-                    ></input>
-                  </fieldset>
-                  <button
-                    className='btn btn-lg btn-primary pull-xsright'
-                    type='submit'
-                  >
-                    Войти
-                  </button>
-                </fieldset>
-              </form>
             </p>
+            <form>
+              <fieldset>
+                <fieldset className='form-group'>
+                  <input
+                    type='email'
+                    className='form-control form-control-lg'
+                    placeholder='Введите email'
+                  ></input>
+                </fieldset>
+                <fieldset className='form-group'>
+                  <input
+                    type='password'
+                    className='form-control form-control-lg'
+                    placeholder='Введите пароль'
+                  ></input>
+                </fieldset>
+                <button
+                  className='btn btn-lg btn-primary pull-xsright'
+                  type='submit'
+                >
+                  Войти
+                </button>
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>
